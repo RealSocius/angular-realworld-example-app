54f36d04f40936c5a96fe61610ec72f5b13b2730
sebastian@hauschildt.dev <sebastian@hauschildt.dev>
2023-10-20 00:15:38 +0200

    adjust update scripts to change src/test.ts

--- update-script-angular-update-tool.sh
+++ update-script-angular-update-tool.sh
@@ -3,3 +3,7 @@
 ng update @angular/core@14 @angular/cli@14 --allow-dirty
 ng update @angular/core@15 @angular/cli@15 --allow-dirty
 ng update @angular/core@16 @angular/cli@16 --allow-dirty
+
+# Update test.ts to make it work -> copied file from angular-13-crud-example
+sed -i 's|'zone.js/dist/long-stack-trace-zone'|'zone.js/testing'|' src/test.ts
+sed -i -e '4,8d;14,20d;29,34d' src/test.ts
--- update-script-versionjump.sh
+++ update-script-versionjump.sh
@@ -1,5 +1,6 @@
 pnpm update --latest
 pnpm i -D typescript@5.1
 
-# Update test.ts to make it work -> copied behaviour from ng update
-sed -i -e '10,16d;22,27d' src/test.ts
+# Update test.ts to make it work -> copied file from angular-13-crud-example
+sed -i 's|'zone.js/dist/long-stack-trace-zone'|'zone.js/testing'|' src/test.ts
+sed -i -e '4,8d;14,20d;29,34d' src/test.ts
